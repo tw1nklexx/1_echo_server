a62dd84c3ccfb481015f6e29398fe3a5ef65e6b9
Nikita Voronov <nikita@gmail.com>
2024-03-26 21:33:19 +0300

    echo-server-nikita

--- client.py
+++ client.py
@@ -1,17 +1,49 @@
 import socket
-from time import sleep
+from typing import Any
 
-port=int(input("port:"))
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', port))
 
-msg = input("Your string:")
-#msg = "Hi!"
-sock.send(msg.encode())
+def connect_to_server(host: str, port: int) -> socket.socket:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print(f'Соединение с сервером {host}:{port} установлено')
+    return client_socket
 
-data = sock.recv(1024)
 
-sock.close()
+def get_user_input(default: Any) -> Any:
+    user_input = input(f'Введите значение (по умолчанию {default}): ')
+    if not user_input:
+        return default
+    return user_input
 
-print(data.decode())
+
+def send_data_to_server(client_socket: socket.socket, data: str) -> None:
+    client_socket.sendall(data.encode())
+    print(f'Отправлено серверу: {data}')
+
+
+def receive_data_from_server(client_socket: socket.socket) -> None:
+    received_data = client_socket.recv(1024).decode()
+    print(f'Получено от сервера: {received_data}')
+
+
+if __name__ == "__main__":
+    HOST = '127.0.0.1'
+    PORT = 12345
+
+    custom_host = get_user_input(HOST)
+    custom_port = int(get_user_input(str(PORT)))
+
+    client_socket: socket.socket = connect_to_server(custom_host, custom_port)
+
+    try:
+        while True:
+            receive_data_from_server(client_socket)
+
+            data: str = input('Введите строку для отправки серверу: ')
+            send_data_to_server(client_socket, data)
+
+            if data == 'exit':
+                break
+
+    finally:
+        client_socket.close()
--- server.py
+++ server.py
@@ -1,27 +1,86 @@
 import socket
+import logging
+from typing import Optional, Union
 
-port=int(input("port:"))
-sock = socket.socket()
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('', port))
-print("Server is starting")
-sock.listen(0)
-print("Port",port,"is listing")
-conn, addr = sock.accept()
-print("Client is accepted")
-print("Client adress:",addr[0])
-print("Client port:",addr[1])
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-msg = ''
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-            print("All data is accepted")
+def get_client_name(client_ip: str) -> Optional[str]:
+    try:
+        with open('clients.txt', 'r') as file:
+            for line in file:
+                ip, name = line.strip().split(',')
+                if ip == client_ip:
+                    return name
+    except FileNotFoundError:
+        pass
+    return None
+
+
+def start_server(host: str, port: int) -> None:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    while True:
+        try:
+            server_socket.bind((host, port))
             break
-	msg += data.decode()
-	conn.send(msg.upper().encode())
+        except OSError:
+            logging.info(f'Порт {port} уже занят, пробуем следующий порт...')
+            port += 1
 
-print(msg)
+    server_socket.listen(1)
+    logging.info(f'Сервер запущен на {host}:{port}')
+    print(f'Сервер запущен на порту {port}')
 
-conn.close()
+    while True:
+        logging.info('Ожидание подключения клиента...')
+        client_socket, client_address = server_socket.accept()
+        client_ip = client_address[0]
+        logging.info(f'Подключен клиент {client_address}')
+
+        client_name = get_client_name(client_ip)
+
+        if not client_name:
+            client_socket.sendall('Представьтесь, пожалуйста: '.encode())
+            client_name = client_socket.recv(1024).decode().strip()
+            logging.info(f'Клиенту {client_address} присвоено имя {client_name}')
+
+            with open('clients.txt', 'a') as file:
+                file.write(f'{client_ip},{client_name}\n')
+
+        welcome_message = f'Добро пожаловать, {client_name}!'
+        client_socket.sendall(welcome_message.encode())
+
+        while True:
+            data = client_socket.recv(1024)
+
+            input_data = data.decode()
+            logging.info(f'Получено от клиента {client_name}: {input_data}')
+
+            if input_data.strip().lower() == 'exit':
+                break
+
+            client_socket.sendall(data)
+            logging.info(f'Отправлено клиенту {client_name}: {input_data}')
+
+        logging.info('Клиент отключен')
+        client_socket.close()
+
+    server_socket.close()
+
+
+def get_user_input(default: Union[str, int]) -> Union[str, int]:
+    user_input = input(f'Введите значение (по умолчанию {default}): ')
+    if not user_input:
+        return default
+    return user_input
+
+
+if __name__ == "__main__":
+    HOST = '127.0.0.1'
+    PORT = 12345
+
+    custom_host = get_user_input(HOST)
+    custom_port = int(get_user_input(str(PORT)))
+
+    start_server(custom_host, custom_port)
